cb7664e66f5aaee2e586459b06c3f7310e623a9e
whoisarjen <Kamilow97@gmail.com>
2022-09-13 21:03:33 +0200

    nie działa

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
 import type { NextPage } from "next";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
-	const { data } = useSession();
+	const { data: session } = useSession();
+console.log(session)
+	useEffect(() => {
+		if (session?.error === "RefreshAccessTokenError") {
+			console.log('refreshed')
+			signIn(); // Force sign in to hopefully resolve error
+		}
+	}, [session]);
+
 	return (
 		<div>
-			<pre>{JSON.stringify(data)}</pre>
+			<pre>{JSON.stringify(session)}</pre>
 			Home
 			<button onClick={() => signIn()}>Sign in</button>
 			<button onClick={() => signOut()}>Sign Out</button>
