3b371880953a159a4a0eb1821c513f01009b03a0
whoisarjen <Kamilow97@gmail.com>
2022-09-13 18:57:23 +0200

    v

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import type { NextPage } from 'next'
+import { useSession, signIn, signOut } from "next-auth/react"
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
+    const { data } = useSession()
+console.log(data)
+    return (
+        <div>
+            Home
+            <button onClick={() => signIn()}>Sign in</button>
+        </div>
+    )
 }
 
 export default Home
