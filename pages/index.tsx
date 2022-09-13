bf8a8bced56fa286f7bdb9d562c88c9dcf48c64d
Adam Jóźwiak <37516296+ne0nowy@users.noreply.github.com>
2022-09-13 20:19:00 +0200

    (update): package.json add studio

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
-import type { NextPage } from 'next'
-import { useSession, signIn, signOut } from "next-auth/react"
+import type { NextPage } from "next";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Home: NextPage = () => {
-    const { data } = useSession()
-console.log(data)
-    return (
-        <div>
-            Home
-            <button onClick={() => signIn()}>Sign in</button>
-        </div>
-    )
-}
+	const { data } = useSession();
+	return (
+		<div>
+			Home
+			<button onClick={() => signIn()}>Sign in</button>
+		</div>
+	);
+};
 
-export default Home
+export default Home;
