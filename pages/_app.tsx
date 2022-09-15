9ad490c0bedff6f2c8d19a9bfd1a7bccd1cd9a48
Adam Jóźwiak <37516296+ne0nowy@users.noreply.github.com>
2022-09-15 21:06:42 +0200

    new(trpc): Added trpc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
 import '../styles/globals.css'
+import { withTRPC } from '@trpc/next';
+import { AppType } from 'next/dist/shared/lib/utils';
+import { AppRouter } from './api/trpc/[trpc]';
 import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
 
@@ -16,4 +19,15 @@ function MyApp({
     )
 }
 
-export default MyApp
+export default withTRPC<AppRouter>({
+    config({ ctx }) {
+      const url = process.env.VERCEL_URL
+        ? `https://${process.env.VERCEL_URL}/api/trpc`
+        : 'http://localhost:3000/api/trpc';
+      return {
+        url,
+      };
+    },
+    ssr: true,
+  })(MyApp);
+
