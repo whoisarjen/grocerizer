3a528a5e30c21bf4543099c646dcd5b43bbf583a
Adam Jóźwiak <37516296+ne0nowy@users.noreply.github.com>
2022-09-15 22:59:24 +0200

    update(trcp): fix cors

--- pages/api/trpc/[trpc].ts
+++ pages/api/trpc/[trpc].ts
@@ -3,6 +3,30 @@ import * as trpc from '@trpc/server';
 import * as trpcNext from '@trpc/server/adapters/next';
 import { z } from 'zod';
 import { pick } from 'lodash'
+import type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
+import Cors from 'cors';
+
+const cors = Cors();
+
+function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+export function withCors(handler: NextApiHandler) {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    await runMiddleware(req, res, cors);
+
+    return await handler(req, res);
+  };
+}
 
 const prisma = new PrismaClient();
 
@@ -40,7 +64,8 @@ export const appRouter = trpc
 // export type definition of API
 export type AppRouter = typeof appRouter;
 // export API handler
-export default trpcNext.createNextApiHandler({
-  router: appRouter,
-  createContext: () => null,
-});
+export default withCors(
+  trpcNext.createNextApiHandler({
+    router: appRouter,
+    createContext: () => null,
+  }))
