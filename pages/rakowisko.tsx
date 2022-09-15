588efe2b952b85e4429de358b91739772cd9714e
Adam Jóźwiak <37516296+ne0nowy@users.noreply.github.com>
2022-09-15 22:42:07 +0200

    update(trpc): remove graphql + test page

--- pages/rakowisko.tsx
+++ pages/rakowisko.tsx
@@ -2,13 +2,23 @@ import type { NextPage } from "next";
 import { trpc } from '../utils/trpc';
 
 const Rakowisko: NextPage = () => {
-    const hello = trpc.useQuery(['hello', { text: 'client' }]);
-    if (!hello.data) {
+    const user = trpc.useQuery(['user', { id: 'cl839q2440227ah0q8046240ov' }]);
+    console.log(user)
+    if (user.isLoading) {
       return <div>Loading...</div>;
     }
+
+    if (user.isError) {
+      return <div>{user.error.message}</div>
+    }
+  
+    if (!user.data) {
+      return null
+    }
+
     return (
       <div>
-        <p>{hello.data.greeting}</p>
+        <p>{user.data.name}</p>
       </div>
     );
 };
